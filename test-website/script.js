7b3a47a15eb53856ee1f863a842c71297d5673d6
Daniel Nilsson <daniel.nilsson1@iths.se>
2024-05-29 17:51:29 +0200

    added more information about basic functionalities of javascript

--- test-website/script.js
+++ test-website/script.js
@@ -1,2 +1,17 @@
-console.log("hello world!");
+// basic javascript functions
+// print statements
 
+console.log("Hello World")
+// semi-colo is optional in many cases in javascript, however sometimes it is required
+
+// Variables and datatypes
+// variables works differently in javascript, you're not specifying datatypes with "var, const or let"
+var name = "Daniel"
+const age = 24
+let isStudent = false
+
+console.log(`Name is ${name}`) // f-string, but for javascript, needs to use backticks for f-strings
+console.log(age)
+console.log(isStudent)
+
+// Basic operations:
